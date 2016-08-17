ebb5b0f8f1d089203be875308a00eaa8a2aca42f
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-17 08:50:42 -0400

    create skeleton of project

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  context: __dirname,
+  entry: "./js/entry.js",
+  output: {
+    path: "./",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js"]
+  },
+  devtool: 'source-map'
+};
