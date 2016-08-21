b53cc9150b4e9fae146b522393b8b1d0782ede21
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-21 16:13:14 -0400

    update code style

--- js/tetris.js
+++ js/tetris.js
@@ -1,12 +1,11 @@
 const Game     = require('./game');
 const GameView = require('./game_view');
-const Board = require('./board');
 
 document.addEventListener("DOMContentLoaded", function () {
   const canvas  = document.getElementById('canvas');
   canvas.width  = Game.DIM_X;
   canvas.height = Game.DIM_Y;
-  const ctx  = canvas.getContext("2d");
-  const game = new Game();
+  const ctx     = canvas.getContext("2d");
+  const game    = new Game();
   new GameView(game, ctx).start();
 });
