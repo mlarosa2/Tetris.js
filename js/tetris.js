22899c12efd1dbdc45ed2d28c9e0cbf89ac1c0a5
Mark LaRosa <mark.larosa2@gmail.com>
2016-08-26 14:43:26 -0400

    remove unused function from window

--- js/tetris.js
+++ js/tetris.js
@@ -10,26 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
   new GameView(game, ctx).start();
 });
 
-window.replay = function () {
-  const canvas  = document.getElementById('canvas');
-  canvas.width  = Game.DIM_X;
-  canvas.height = Game.DIM_Y;
-  const ctx     = canvas.getContext("2d");
-  const game    = new Game();
-  new GameView(game, ctx).start();
-};
-
-
 let mute  = document.getElementById("volume");
 let audio = document.getElementById("theme");
 
 mute.addEventListener('click', function () {
   if (!audio.paused) {
-    mute.innerHTML = '<span class="fa-stack"><i class="fa fa-music fa-stack-1x"></i><i class="fa fa-ban fa-stack-1x"></i></span>&nbsp;&nbsp;Unmute</span>'
+    mute.innerHTML = '<span class="fa-stack"><i class="fa fa-music fa-stack-1x"></i><i class="fa fa-ban fa-stack-1x"></i></span> Unmute</span>'
     mute.className = 'muted'
     audio.pause();
   } else {
-    mute.innerHTML = '<i class="fa fa-music"></i>&nbsp;&nbsp;Mute';
+    mute.innerHTML = '<i class="fa fa-music"></i> Mute';
     mute.className = '';
     audio.play();
   }
